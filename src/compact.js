5dcc72e4ddb33b51e6d150d81e20f743f5b2f788
Andrew Huth <ahuth@mavenlink.com>
2018-11-23 22:00:08 -0800

    Whitespace

--- src/compact.js
+++ src/compact.js
@@ -1,9 +1,11 @@
 export default function compact(object) {
   const cloned = {};
+
   for (let property in object) {
     if (object.hasOwnProperty(property) && object[property] != null) {
       cloned[property] = object[property];
     }
   }
+
   return cloned;
 }
